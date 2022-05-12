218d6e51be1255f7b741c61d65c116bdbb350184
cynamon jules <julescweb@mail.com>
2022-05-12 08:53:25 +0200

    projet finis

--- src/Pages/DashboardFinance/DashboardFinance.js
+++ src/Pages/DashboardFinance/DashboardFinance.js
@@ -1,7 +1,7 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import "./DashboardFinance.css";
 import BarChart from "../../Components/AllChart/Barchart";
-// import LineChart from "../../Components/AllChart/LineChart";
+import LineChart from "../../Components/AllChart/LineChart";
 import { DashboardContext } from "../../context/DashboardContext";
 
 export default function DashboardFinance() {
@@ -14,14 +14,46 @@ export default function DashboardFinance() {
 			<form>
 				<label htmlFor='year'>Choisissez une date</label>
 				<select onChange={changeYear} id='year'>
-					<option value='2020'>2020</option>
-					<option value='2019'>2019</option>
-					<option value='2018'>2018</option>
-					<option value='2017'>2017</option>
+					<option
+						selected={yearData === "2020" && "selected"}
+						value='2020'
+					>
+						2020
+					</option>
+					<option
+						selected={yearData === "2019" && "selected"}
+						value='2019'
+					>
+						2019
+					</option>
+					<option
+						selected={yearData === "2018" && "selected"}
+						value='2018'
+					>
+						2018
+					</option>
+					<option
+						selected={yearData === "2017" && "selected"}
+						value='2017'
+					>
+						2017
+					</option>
 				</select>
 			</form>
 			<div className='dashboard-container'>
-				<BarChart data={dataChart.chart1} name={"Chiffres biméstriels"} />
+				<BarChart
+					data={dataChart.chart1}
+					name={"Chiffres biméstriels"}
+				/>
+				<LineChart
+					data={dataChart.chart2}
+					name={"Nombre d'abonnés (en milliers)"}
+				/>
+				<LineChart
+					data={dataChart.chart3}
+					name={"Nombre de clients (en milliers)"}
+				/>
+				<BarChart data={dataChart.chart4} name={"Budget marketing"} />
 			</div>
 		</div>
 	);
